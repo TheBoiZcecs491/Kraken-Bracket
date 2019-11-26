d25c5c0cce870ba5a2a552551904c75431447142
b-rye-the-asian-spy <brian.edison.nguyen@gmail.com>
2019-11-26 12:16:59 -0800

    Added method used to retrieve email and password

--- Authentication/DataAccess/DataAccess.cs
+++ Authentication/DataAccess/DataAccess.cs
@@ -1,13 +1,49 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Data.AccessLayer
 {
     public class DataAccess
     {
-        public int GetEmailAndPassword(string email, string password)
+        const string CONNECTION_STRING = @"Data source=localhost; Database=kraken_bracket; User ID=root; Password=Gray$cale917!!";
+        public bool GetEmailAndPassword(string email, string password)
         {
+            string queryString = "SELECT * FROM User WHERE Email='" + email + "' AND Password='" + password + "'";
+            MySqlConnection conn = new MySqlConnection(CONNECTION_STRING);
+            MySqlCommand searchCmd = new MySqlCommand(queryString, conn);
+            MySqlDataReader reader;
 
-            return 0;
+            int count = 0;
+            conn.Open();
+            reader = searchCmd.ExecuteReader();
+            while (reader.Read()) { count++; }
+            if (count == 1)
+            {
+                return true;
+            }
+            else
+            {
+                throw new ArgumentException("Email or password does not exist");
+            }
+
+        }
+
+        [TestMethod]
+        public void TestConnection()
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(CONNECTION_STRING);
+                conn.Open();
+                Console.WriteLine("Connection successful!!!");
+                conn.Close();
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+
         }
     }
 }
