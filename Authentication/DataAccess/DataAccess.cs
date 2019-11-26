1daf2de839f42e42d9d4cb254387be3cfc25a0b0
b-rye-the-asian-spy <brian.edison.nguyen@gmail.com>
2019-11-26 14:39:19 -0800

    Added comments

--- Authentication/DataAccess/DataAccess.cs
+++ Authentication/DataAccess/DataAccess.cs
@@ -7,17 +7,27 @@ namespace Data.AccessLayer
     public class DataAccess
     {
         const string CONNECTION_STRING = @"Data source=localhost; Database=kraken_bracket; User ID=root; Password=Gray$cale917!!";
+
+        /// <summary>
+        /// Method used to check if email and password used.
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns>
+        /// True if both email and password exist. False if at least 1 does not.
+        /// </returns>
         public bool GetEmailAndPassword(string email, string password)
         {
             string queryString = "SELECT * FROM User WHERE Email='" + email + "' AND Password='" + password + "'";
             MySqlConnection conn = new MySqlConnection(CONNECTION_STRING);
             MySqlCommand searchCmd = new MySqlCommand(queryString, conn);
             MySqlDataReader reader;
-
             int count = 0;
+
             conn.Open();
             reader = searchCmd.ExecuteReader();
             while (reader.Read()) { count++; }
+            conn.Close();
             if (count == 1)
             {
                 return true;
@@ -26,9 +36,11 @@ namespace Data.AccessLayer
             {
                 throw new ArgumentException("Email or password does not exist");
             }
-
         }
 
+        /// <summary>
+        /// Method used to test the MySQL connection of the database.
+        /// </summary>
         [TestMethod]
         public void TestConnection()
         {
