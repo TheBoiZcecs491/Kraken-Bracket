10835e6b8a922cf8815af6a5c9e70e55b1085144
b-rye-the-asian-spy <brian.edison.nguyen@gmail.com>
2019-11-26 14:38:26 -0800

    Init methods GetClaim() and GenerateToken()

--- Authentication/AuthenticationService/AuthenticationService.cs
+++ Authentication/AuthenticationService/AuthenticationService.cs
@@ -7,8 +7,36 @@ namespace Authentication.Services
     {
         public void AuthenticateUser(string email, string password)
         {
+            try
+            {
+                var dataAccess = new DataAccess();
+                bool found = dataAccess.GetEmailAndPassword(email, password);
+                if (found == true)
+                {
+                    GetClaim(email);
+                }
+                else
+                {
+
+                }
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae);
+            }
+            catch (Exception) { }
+        }
+
+        public void GetClaim(string email)
+        {
             var dataAccess = new DataAccess();
-            dataAccess.GetEmailAndPassword(email, password);
+
+        }
+        public string GenerateToken(string email, string password, string claim)
+        {
+            string temp = "";
+
+            return temp;
         }
     }
 }
