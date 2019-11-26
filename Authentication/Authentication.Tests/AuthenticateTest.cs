96a1f50db970be73fad2063bcd20116c3dc4ec02
b-rye-the-asian-spy <brian.edison.nguyen@gmail.com>
2019-11-26 14:36:50 -0800

    Added fail test method

--- Authentication/Authentication.Tests/AuthenticateTest.cs
+++ Authentication/Authentication.Tests/AuthenticateTest.cs
@@ -10,7 +10,14 @@ namespace Authentication.Tests
         public void AuthenticateUser_Pass()
         {
             var authenticationService = new AuthenticationService();
-            authenticationService.AuthenticateUser("brian@fmail.com", "password");
+            authenticationService.AuthenticateUser("brian@foomail.com", "123");
+        }
+
+        [TestMethod]
+        public void AuthenticateUser_failed()
+        {
+            var authenticationService = new AuthenticationService();
+            authenticationService.AuthenticateUser("brian@goomail.com", "111");
         }
     }
 }
