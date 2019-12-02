59e2f547e4976a3ccaca59392e8cb61f93166b69
DThorin <48812658+DThorin@users.noreply.github.com>
2019-12-02 12:25:46 -0800

    Add files via upload

---
+++ LoggingTest.cs
@@ -0,0 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TBZ_Logging;
+
+namespace TBZ_LoggingTest
+{
+    [TestClass]
+    public class LoggingTest
+    {
+        [TestMethod]
+        public void Log_CreateObj_Pass()
+        {
+            //Arrange
+            Boolean result = true;
+            String path = "";
+            //Act
+            try
+            {
+                var a = new Logging(path);
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Log_CreateObj_Fail()
+        {
+            //Arrange
+            Boolean result = true;
+            String path = "";
+            //Act
+            try
+            {
+                var a = new Logging(path);
+            }
+            catch (Exception e)
+            {
+                result = false;
+            }
+            //Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
