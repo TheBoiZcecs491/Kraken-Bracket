2ff722546a69ff9ef8c2724b4dfa7eead64841d3
DThorin <48812658+DThorin@users.noreply.github.com>
2019-12-02 12:34:33 -0800

    Add files via upload

---
+++ Milestone-2/Archiving/TBZ_Archiving/Archiving.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace TBZ_Archiving
+{
+    public class Archiving
+    {
+        public Archiving()
+        {
+
+        }
+
+        public void Archive()
+        {
+            String path = "";       // src from logging
+            String path2 = "";      // dest according to YYYYMMDD
+            File.Copy(path, path2); // copy from src to dest
+        }
+    }
+}
---
+++ Milestone-2/Archiving/TBZ_ArchivingTest/ArchivingTest.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TBZ_Archiving;
+
+namespace TBZ_ArchivingTest
+{
+    [TestClass]
+    public class ArchivingTest
+    {
+        [TestMethod]
+        public void Archiving_CreateObj_Pass()
+        {
+            //Arrange
+            Boolean result = true;
+            //Act
+            try
+            {
+                var a = new Archiving();
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            //Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
