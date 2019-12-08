ed6f5ada48bb86a2fae4fcd4ec615f81923abfd7
Brian Nguyen <brian.edison.nguyen@gmail.com>
2019-12-08 15:38:19 -0800

    Reworked dictionary of claims

--- Milestone-2/TBZ_Authentication/TBZ_DataAccess/DataAccess.cs
+++ Milestone-2/TBZ_Authentication/TBZ_DataAccess/DataAccess.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Security.Claims;
+using System.Linq;
 
 namespace Data.AccessLayer
 {
     public class DataAccess
     {
+        // List of users and their passwords
         Dictionary<string, string> userDict = new Dictionary<string, string>()
         {
             {"brian@foomail.com", "123"},
             {"test@fmail.com", "legoMyEggo123"}
         };
-        Dictionary<string, string> claims = new Dictionary<string, string>()
-        {
-            {"brian@foomail.com", new Claim("http://kraken-bracket.gg", "Host").ToString()},
-            {"test@fmail.com", new Claim("http://kraken-bracket.gg", "Co-Host").ToString()}
-        };
 
+        // Initialize dictionary with email (string) as keys and list of claims (List<string> as the values
+        Dictionary<string, List<string>> claims = new Dictionary<string, List<string>>()
+        {
+            {"brian@foomail.com", new List<string>(){ "brian@foomail.com", "Host"} },
+            {"test@fmail.com", new List<string>(){ "test@fmail.com", "Co-Host"} },
+        };
 
         /// <summary>
         /// Method used to check if email and password used.
@@ -30,8 +32,6 @@ namespace Data.AccessLayer
         /// </returns>
         public bool GetEmailAndPassword(string email, string password)
         {
-            
-
             // Checks if email exists in dictionary
             if (userDict.TryGetValue(email, out string value))
             {
@@ -50,22 +50,22 @@ namespace Data.AccessLayer
         /// <param name="email"></param>
         /// <returns></returns>
         /// 
-
-        // FIXME: Research if this is how to assign claims
         public string DSGetClaim(string email)
         {
+            // var userPrincipal = new ClaimsPrincipal(new[] { brianIdentity }).ToString();
             if (claims.ContainsKey(email))
             {
                 // TryGetValue(string key, out string value) - gets value associated with key.
-                // The statement below will return the claim associated to the email address.
-                claims.TryGetValue(email, out string value).ToString();
-                return value;
+                // The statement below will return the claims associated to the email address.
+                claims.TryGetValue(email, out List<string> value).ToString();
+                // Convert the list of claims into an array
+                string claimCollection = string.Join(",", value.ToArray());
+                return claimCollection;
             }
             else
             {
                 throw new Exception();
             }
         }
-
     }
 }
