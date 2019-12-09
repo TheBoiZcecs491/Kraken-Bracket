9a30c58639fb9bfd30dfe2fbedfcb04389381699
Kevin Phan <keviinphan@gmail.com>
2019-12-09 13:35:45 -0800

    Working on checking action string
    Adding actions for Host/Co-Host users

--- Milestone-2/AuthorizationService/AuthorizationService.cs
+++ Milestone-2/AuthorizationService/AuthorizationService.cs
@@ -5,11 +5,30 @@ namespace AuthorizationService
 {
     public class AuthorizationService
     {
-        public bool UserPermission(string email)
+        public bool UserPermission(string email, string action, bool isLoggedIn)
         {
             var dataAccess = new DataAccess();
-            string permissionLevel;
+            bool permission = false;
             string claim = dataAccess.DSGetClaim(email);
+            if(isLoggedIn)
+            {
+                if((action == "Update Event Information") ||
+                   (action == "Update Tournament Bracket Information") ||
+                   (action == "Manage Tournament Bracket") ||
+                   (action == "Delete Tournament Bracket and Event") ||
+                   (action == "Assign Other Registered Users To Be A Co- Host"))
+                {
+                    if (claim == "Host" || claim == "Co-Host")
+                        permission = true;
+                    return permission;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("User is not authorized to perform: " + action);
+                return permission;
+            }
 
         }
     }
