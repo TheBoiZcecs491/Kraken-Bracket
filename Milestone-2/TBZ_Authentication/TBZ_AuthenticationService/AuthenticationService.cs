def3d75d9e2c736c7fe8ed77a33017fb629983af
b-rye-the-asian-spy <brian.edison.nguyen@gmail.com>
2019-12-10 14:45:54 -0800

    Added comments

--- Milestone-2/TBZ_Authentication/TBZ_AuthenticationService/AuthenticationService.cs
+++ Milestone-2/TBZ_Authentication/TBZ_AuthenticationService/AuthenticationService.cs
@@ -22,22 +22,36 @@ namespace Authentication.Services
         /// </returns>
         public string AuthenticateUser(string email, string password)
         {
+            // Check to see if passed-in email or password is null / empty
             if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
             {
                 throw new ArgumentException("Email or password cannot be null / empty");
             }
-            var dataAccess = new DataAccess();
-            bool found = dataAccess.GetEmailAndPassword(email, password);
-            string hashedPassword = GetHashedPassword(password);
+
+            // Declare claim and token string variables
             string claim, token;
-            // string key;
+
+            // Initialize DataAccess object
+            var dataAccess = new DataAccess();
+
+            // Check to see if email / password combination exists in the datastore
+            bool found = dataAccess.GetEmailAndPassword(email, password);
+            
+            // If the email and password combination exists in the database
             if (found == true)
             {
+                // Retrieve hashed password
+                string hashedPassword = GetHashedPassword(password);
+
+                // Retrieve claim associated with user
                 claim = dataAccess.DSGetClaim(email);
+
+                // Generate token to assign to user
                 token = GenerateToken(email, hashedPassword, claim);
-                // key = DecodeToken(token);
                 return token;
             }
+
+            // The email / password combination does not exist in the datastore
             else
             {
                 throw new ArgumentException("No results returned");
@@ -62,7 +76,7 @@ namespace Authentication.Services
         /// </returns>
         public string GenerateToken(string email, string password, string claim)
         {
-            // NOTE: string.Join() concatenates all elements in a string array.
+            // Concatenate email, (hashed) password, and claim in a string array.
             string hash = string.Join(":", new string[] { email, password, claim });
             string hashLeft = "";
             string hashRight = "";
@@ -77,14 +91,31 @@ namespace Authentication.Services
             return token;
         }
 
+        /// <summary>
+        /// Method to decode a user's token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns>
+        /// Decoded token in the format of HashedPassword:Email:Role
+        /// </returns>
         public string DecodeToken(string token)
         {
             string key = Encoding.UTF8.GetString(Convert.FromBase64String(token));
             return key;
         }
 
+        /// <summary>
+        /// HMAC SHA-256 algorithm to hash a password.
+        /// 
+        /// Algorithm found on the following website:
+        /// http://www.primaryobjects.com/2015/05/08/token-based-authentication-for-web-service-apis-in-c-mvc-net/
+        /// 
+        /// </summary>
+        /// <param name="password">(Unhashed) Password to be hashed</param>
+        /// <returns>Hashed password</returns>
         public static string GetHashedPassword(string password)
         {
+            // Concatenate password and salt
             string key = string.Join(":", new string[] { password, _salt });
             using (HMAC hmac = HMACSHA256.Create(_algorithm))
             {
