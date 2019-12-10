514acd2e02741017d014608c8a544ec14d622e62
DThorin <48812658+DThorin@users.noreply.github.com>
2019-12-10 12:14:02 -0800

    Add files via upload
    Update features of logging

--- Milestone-2/TBZ_Logging/TBZ_Logging/Logging.cs
+++ Milestone-2/TBZ_Logging/TBZ_Logging/Logging.cs
@@ -4,25 +4,93 @@ namespace TBZ_Logging
 {
     public class Logging
     {
-        static String path;
-        public Logging(String doc) { path = doc; }
+        static string path;
+        static int tries;
 
         /// <summary>
-        /// 
+        /// A logging object requires a document path to save logs.
         /// </summary>
-        // TODO: consider async Task<bool[]> Log()
-        public void Log(String operation, String msg, String id) {
-            DateTime timestamp = DateTime.UtcNow;
-            String time = timestamp.ToString("s");
-            String finallog;
-            if (msg.Equals("")){
-                finallog = time + " " + operation;
-            }
-            else
+        /// <param name="doc"> String of document path </param>
+        /// <param name="runs"> Integer of the number of attempts to try to log </param>
+        public Logging(string doc, int runs) { path = doc; tries = runs; }
+
+        /// <summary>
+        /// A helper method to date and time stamp the current log using DateTime struct.
+        /// </summary>
+        /// <returns> String of the current date and time </returns>
+        protected string TimeStamp()
+        {
+            DateTime timestamp = DateTime.UtcNow;                           // Current Date & Time (UTC)
+            string time = timestamp.ToString("yyyy-MM-dd, HH:mm:ss:ff");    // Format: Year-Month-Day, Hour:Minute:Second:Milisecond
+            return time;
+        }
+
+        /// <summary>
+        /// A helper method to format the contents of a log.
+        /// </summary>
+        /// <param name="operation"> String of logged operation </param>
+        /// <param name="msg"> String of error message (if an error occurred), "" otherwise </param>
+        /// <param name="id"> String of User/System Unique Identification </param>
+        /// <param name="time"> String of the current date and time from TimeStamp </param>
+        /// <returns> String of the correctly formatted log contents </returns>
+        protected string FormatLog(string operation, string msg, string id, string time)
+        {
+            string finallog;    // separated every value by comma (leave empty space for error message)
+            if (msg.Equals("")) { finallog = time + ", " + operation + ", , ID_" + id + ",\n"; }   // Format: Date, Time, Operation, , ID_
+            else { finallog = time + ", " + operation + ", \"" + msg + "\", ID_" + id + ",\n"; }  // Format: Date, Time, Operation, "Error", ID_
+            return finallog;
+        }
+
+        /// <summary>
+        /// A helper method to append the log to the desired file.
+        /// </summary>
+        /// <param name="log"> String of the correctly formatted log contents from FormatLog </param>
+        /// <returns> Boolean of success or failure to log </returns>
+        protected bool AppendLog(string log)
+        {
+            bool success = false;
+            for (int i = 0; i < tries; i++)
             {
-                finallog = time + " " + operation + " " + msg;
+                try
+                {
+                    File.AppendAllText(path, log);      // Appends to a new file if one does not
+                    success = true;                     // previously exist for this file path
+                    break;
+                }
+                catch
+                {
+                    Console.Write("Path Failed " + (i + 1) + " Time(s).\n");
+                    success = false;
+                }
             }
-            File.AppendAllText(path, finallog);
+            return success;
+        }
+
+        /// <summary>
+        /// A logging object can "log" with a specific operation,
+        /// error message (if applicable), and a user id.
+        /// </summary>
+        /// <param name="operation"> String of logged operation </param>
+        /// <param name="msg"> String of error message (if an error occurred), "" otherwise </param>
+        /// <param name="id"> String of User/System Unique Identification </param>
+        /// <returns> Boolean of success or failure to log </returns>
+        // TODO: consider async Task<bool[]> Log()
+
+        public bool Log(string operation, string msg, string id) {
+            bool success;
+            //test operation and id for validity
+            if (string.IsNullOrWhiteSpace(operation) || string.IsNullOrWhiteSpace(id))
+            {
+                throw new ArgumentNullException();
+            }
+            if (tries < 1)
+            {
+                throw new ArgumentException();
+            }
+            string time = TimeStamp();
+            string log = FormatLog(operation, msg, id, time);
+            success = AppendLog(log);
+            return success;
         }
     }
 }
