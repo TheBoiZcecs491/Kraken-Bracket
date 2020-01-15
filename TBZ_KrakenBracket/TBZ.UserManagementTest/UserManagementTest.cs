a8e90ae84f4699300160329ba21c0ed962ebc01c
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-15 11:12:10 -0800

    feat: intial user management test class

---
+++ TBZ_KrakenBracket/TBZ.UserManagementTest/UserManagementTest.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TBZ.UserManagementTest
+{
+    [TestClass]
+    public class UserManagementTest
+    {
+        [TestMethod]
+        public void CreateUsers_Pass()
+        {
+            try
+            {
+
+            }
+            catch(ArgumentException)
+            {
+
+            }
+        }
+    }
+}
