d1378a443e7bb4fa1c68f258d6dc9dab39177f0a
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-15 10:48:46 -0800

    feat: random password generator and CreateUsers() method

--- TBZ_KrakenBracket/TBZ.UserManagementService/UserManagement.cs
+++ TBZ_KrakenBracket/TBZ.UserManagementService/UserManagement.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using System.Linq;
+using TBZ.DatabaseAccess;
 
 namespace TBZ.UserManagementService
 {
     public class UserManagement
     {
+        private int systemID = 0;
+        private Random random = new Random();
+        public void CreateUsers(int amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Amount must be greater than zero");
+            }
+            for (int i = 0; i < amount; i ++)
+            {
+                systemID++;
+                // TODO: store user and their password
+            }
+        }
+
+        public string RandomString(int len)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, len)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
     }
 }
