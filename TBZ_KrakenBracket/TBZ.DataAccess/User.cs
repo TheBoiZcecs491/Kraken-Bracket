67622eb7811dc3107b32ee92c660cbde6a0ee55a
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-18 10:55:54 -0800

    Initial User class

---
+++ TBZ_KrakenBracket/TBZ.DataAccess/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TBZ.DatabaseAccess
+{
+    public class User
+    {
+        public int SystemID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string AccountType { get; set; }
+    }
+}
