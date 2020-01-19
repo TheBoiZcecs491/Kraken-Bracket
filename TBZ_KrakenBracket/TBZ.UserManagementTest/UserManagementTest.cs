1b4330ef0d46ac5b8a1dc68e4bdb30f97d45ae25
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-19 12:09:50 -0800

    fix: changed test methods to reflect changes on enable/disable and delete users

--- TBZ_KrakenBracket/TBZ.UserManagementTest/UserManagementTest.cs
+++ TBZ_KrakenBracket/TBZ.UserManagementTest/UserManagementTest.cs
@@ -85,11 +85,12 @@ namespace TBZ.UserManagementTest
         {
             var userManagement = new UserManagement();
             bool result = false;
-            // System ID #2 is an admin. Rest are Users
-            List<int> listOfIDs = new List<int>() { 2, 3, 4, 5 };
+            // Attempting to delete any account should fail since the current account
+            // is a User
+            int[] listOfIDs = { 1, 2, 3, 4, 5 };
             try
             {
-                userManagement.DeleteUsers(listOfIDs, "User");
+                bool[] actual = userManagement.DeleteUsers(listOfIDs, "User");
             }
             catch (ArgumentException)
             {
@@ -104,84 +105,48 @@ namespace TBZ.UserManagementTest
         public void DeleteUsers_SystemAdmin_Pass()
         {
             var userManagement = new UserManagement();
-            bool result = true;
-
-            // System ID #2 is an admin. Rest are Users
-            List<int> listOfIDs = new List<int>() {2, 3, 4, 5};
-            try
-            {
-                userManagement.DeleteUsers(listOfIDs, "System Admin");
-            }
-            catch (ArgumentException)
-            {
-                result = false;
-            }
-            catch (Exception) { }
-
-            Assert.IsTrue(result);
+            // System ID's #1 and #2 is are system admin and admin. Rest are Users
+            int[] listOfIDs = { 1, 2, 3, 4, 5 };
+            bool[] expected = { false, true, true, true, true };
+            bool[] actual = userManagement.DeleteUsers(listOfIDs, "System Admin");
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void DeleteUsers_Admin_Pass()
         {
             var userManagement = new UserManagement();
-            bool result = true;
-
-            // System ID #2 is an admin. Rest are Users
-            List<int> listOfIDs = new List<int>() {2, 3, 4, 5 };
-            try
-            {
-                userManagement.DeleteUsers(listOfIDs, "Admin");
-            }
-            catch (ArgumentException)
-            {
-                result = false;
-            }
-            catch (Exception) { }
-
-            Assert.IsTrue(result);
+            // System ID's #1 and #2 is are system admin and admin. Rest are Users
+            int[] listOfIDs = { 1, 2, 3, 4, 5 };
+            bool[] expected = { false, false, true, true, true };
+            bool[] actual = userManagement.DeleteUsers(listOfIDs, "System Admin");
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void DeleteUsers_Admin_Fail_UserIsAdmin()
+        public void EnableUsers_SystemAdmin_Pass()
         {
             var userManagement = new UserManagement();
-            bool result = true;
 
-            // System ID #2 is an admin
-            List<int> listOfIDs = new List<int>() {2};
-            try
-            {
-                userManagement.DeleteUsers(listOfIDs, "Admin");
-            }
-            catch (ArgumentException)
-            {
-                result = false;
-            }
-            catch (Exception) { }
-
-            Assert.IsTrue(result);
+            // System ID's #1 and #2 is are system admin and admin respectively. Rest are users.
+            // System #4 is the only account disabled
+            int[] listOfIDs =  {1, 2, 3, 4, 5};
+            bool[] expected = { false, false, false, true, false };
+            bool[] actual = userManagement.EnableUsers(listOfIDs, "System Admin");
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void DeleteUsers_Admin_Fail_UserIsSystemAdmin()
+        public void DisableUsers_SystemAdmin_Pass()
         {
             var userManagement = new UserManagement();
-            bool result = true;
 
-            // System ID #2 is an admin
-            List<int> listOfIDs = new List<int>() { 1 };
-            try
-            {
-                userManagement.DeleteUsers(listOfIDs, "System Admin");
-            }
-            catch (ArgumentException)
-            {
-                result = false;
-            }
-            catch (Exception) { }
-
-            Assert.IsTrue(result);
+            // System ID's #1 and #2 is are system admin and admin respectively. Rest are users.
+            // System #4 is the only account disabled
+            int[] listOfIDs = { 1, 2, 3, 4, 5 };
+            bool[] expected = { true, true, true, false, true };
+            bool[] actual = userManagement.DisableUsers(listOfIDs, "System Admin");
+            CollectionAssert.AreEqual(expected, actual);
         }
     }
 }
