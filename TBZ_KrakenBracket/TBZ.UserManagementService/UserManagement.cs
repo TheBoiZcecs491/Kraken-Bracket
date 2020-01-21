104247e48fa78736f57353a643ef8d95635a5d16
b-rye-the-asian-spy <brian.edison.nguyen@gmail.com>
2020-01-21 15:15:21 -0800

    user management service comments

--- TBZ_KrakenBracket/TBZ.UserManagementService/UserManagement.cs
+++ TBZ_KrakenBracket/TBZ.UserManagementService/UserManagement.cs
@@ -12,6 +12,10 @@ namespace TBZ.UserManagementService
 
         List<string> permissions = new List<string>(new string[] { "Admin", "System Admin" });
 
+        /// <summary>
+        /// Method to check account permission
+        /// </summary>
+        /// <param name="permission">Permisison associated with account</param>
         public void CheckPermission(string permission)
         {
             if (!permissions.Contains(permission))
@@ -20,6 +24,16 @@ namespace TBZ.UserManagementService
             }
         }
 
+        /// <summary>
+        /// Method used with bulk storing users. Checks the amount of Users and Admins the 
+        /// current account wishes to store
+        /// </summary>
+        /// <param name="amountOfUsers">Amount of users</param>
+        /// <param name="amountOfAdmins">Amount of admins</param>
+        /// <param name="permission">
+        /// Permisison associated with
+        /// the account that is performing the operation
+        /// </param>
         public void CheckAmount(int amountOfUsers, int amountOfAdmins, string permission)
         {
             if (amountOfUsers <= 0 && amountOfAdmins <= 0)
@@ -32,6 +46,10 @@ namespace TBZ.UserManagementService
             }
         }
 
+        /// <summary>
+        /// Method for checking if list length is valid
+        /// </summary>
+        /// <param name="list"></param>
         public void CheckListLength(int[] list)
         {
             if (list.Length < 1)
@@ -40,7 +58,20 @@ namespace TBZ.UserManagementService
             }
         }
 
-        public void SingleCreateUsers(int sysID, string firstName, string lastName,
+        /// <summary>
+        /// Method for creating a single user
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <param name="accountType"></param>
+        /// <param name="accountStatus"></param>
+        /// <param name="permission">
+        /// Permisison associated with
+        /// the account that is creating the user
+        /// </param>
+        public void SingleCreateUsers(string firstName, string lastName,
             string email, string password, string accountType, bool accountStatus, string permission)
         {
             // TODO: have a check for password. Use Kevin's registration checker
@@ -48,7 +79,7 @@ namespace TBZ.UserManagementService
             var dataAccess = new DataAccess();
             if (permission == "Admin" && accountType == "User" || ((permission == "System Admin" && accountType != "System Admin")))
             {
-                dataAccess.StoreUser(sysID, firstName, lastName, email, password, accountType, accountStatus);
+                dataAccess.StoreUser(firstName, lastName, email, password, accountType, accountStatus);
             }
             else
             {
@@ -56,6 +87,13 @@ namespace TBZ.UserManagementService
             }
         }
 
+        /// <summary>
+        /// Method for bulk creating users
+        /// </summary>
+        /// <param name="amountOfUsers">Amount of regular Users</param>
+        /// <param name="amountOfAdmins">Amount of Admins</param>
+        /// <param name="permission">Permisison associated with
+        /// the account that is creating the user</param>
         public void BulkCreateUsers(int amountOfUsers, int amountOfAdmins, string permission)
         {
             CheckPermission(permission);
@@ -69,7 +107,7 @@ namespace TBZ.UserManagementService
 
                     // The emails will be retrieved from a list later on
                     // For now, they will be stored as null
-                    dataAccess.StoreUser(0, null, null, null, randomPassword, "User", true);
+                    dataAccess.StoreUser(null, null, null, randomPassword, "User", true);
                 }
             }
 
@@ -83,7 +121,7 @@ namespace TBZ.UserManagementService
                     randomPassword = RandomPassword(14);
                     // The emails will be retrieved from a list later on
                     // For now, they will be stored as null
-                    dataAccess.StoreUser(0, null, null, null, randomPassword, "User", true);
+                    dataAccess.StoreUser(null, null, null, randomPassword, "User", true);
                 }
 
                 // Store admins
@@ -91,12 +129,24 @@ namespace TBZ.UserManagementService
                 {
                     randomPassword = RandomPassword(14);
                     // The emails will be retrieved from a list later on
-                    dataAccess.StoreUser(0, null, null, null, randomPassword, "Admin", true);
+                    dataAccess.StoreUser(null, null, null, randomPassword, "Admin", true);
                 }
             }
 
         }
 
+        /// <summary>
+        /// Method for deleting a single user
+        /// </summary>
+        /// <param name="ID">ID of account to be deleted</param>
+        /// <param name="permission">
+        /// Permisison associated with
+        /// the account that is creating the user
+        /// </param>
+        /// <returns>
+        /// True if account is deleted.
+        /// False if account is not deleted
+        /// </returns>
         public bool SingleDeleteUser(int ID, string permission)
         {
             CheckPermission(permission);
@@ -104,6 +154,16 @@ namespace TBZ.UserManagementService
             return dataAccess.DeleteUser(ID, permission);
         }
 
+        /// <summary>
+        /// Method for bulk deleting users
+        /// </summary>
+        /// <param name="listOfIDs"></param>
+        /// <param name="permission">
+        /// Permisison associated with
+        /// the account that is performing the operation</param>
+        /// <returns>
+        /// List of booleans for each account. True if operation succeded; false if not
+        /// </returns>
         public bool[] BulkDeleteUsers(int[] listOfIDs, string permission)
         {
             CheckPermission(permission);
@@ -120,12 +180,28 @@ namespace TBZ.UserManagementService
             return b;
         }
 
+        /// <summary>
+        /// Method for enabling a single user
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <param name="permission">
+        /// Permisison associated with
+        /// the account that is performing the operation
+        /// </param>
+        /// <returns>True if operation is successful; false if not</returns>
         public bool SingleEnableUser(int ID, string permission)
         {
             CheckPermission(permission);
             var dataAccess = new DataAccess();
             return dataAccess.EnableUser(ID, permission);
         }
+
+        /// <summary>
+        /// Method for bulk enabling users
+        /// </summary>
+        /// <param name="listOfIDs">List of system ID's of users to be enabled</param>
+        /// <param name="permission">Permission associated with account performing operation</param>
+        /// <returns></returns>
         public bool[] BulkEnableUsers(int[] listOfIDs, string permission)
         {
             CheckPermission(permission);
@@ -142,12 +218,24 @@ namespace TBZ.UserManagementService
             return b;
         }
 
+        /// <summary>
+        /// Method for disabling a single user
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <param name="permission"></param>
+        /// <returns>True if operation is successful; false if not</returns>
         public bool SingleDisableUser(int ID, string permission)
         {
             CheckPermission(permission);
             var dataAccess = new DataAccess();
             return dataAccess.DisableUser(ID, permission);
         }
+        /// <summary>
+        /// Method for bulk disabling users
+        /// </summary>
+        /// <param name="listOfIDs"></param>
+        /// <param name="permission"></param>
+        /// <returns>List of booleans for each account. True if operation succeded; false if not</returns>
         public bool[] BulkDisableUsers(int[] listOfIDs, string permission)
         {
             CheckPermission(permission);
@@ -164,6 +252,21 @@ namespace TBZ.UserManagementService
             return b;
         }
 
+        /// <summary>
+        /// Method for updating a single user
+        /// </summary>
+        /// <param name="sysID"></param>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <param name="accountType"></param>
+        /// <param name="component">Attribute of user that is to be changed</param>
+        /// <param name="permission">
+        /// Permisison associated with
+        /// the account that is performing the operation
+        /// </param>
+        /// <returns>True if operation is successful; false if not</returns>
         public bool SingleUpdateUser(int sysID, string firstName, string lastName,
             string email, string password, string accountType, string component, string permission)
         {
@@ -172,6 +275,11 @@ namespace TBZ.UserManagementService
             return dataAccess.UpdateUser(sysID, firstName, lastName, email, password, accountType, component, permission);
         }
 
+        /// <summary>
+        /// Random password generator
+        /// </summary>
+        /// <param name="len">Length of random password</param>
+        /// <returns>Random password</returns>
         public string RandomPassword(int len)
         {
             const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
