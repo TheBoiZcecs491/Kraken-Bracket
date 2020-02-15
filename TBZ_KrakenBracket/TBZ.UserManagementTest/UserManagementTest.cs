ffb87d271eb8809cee087cf06a92dab9eac46ceb
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-02-15 15:58:12 -0800

    new test methods

--- TBZ_KrakenBracket/TBZ.UserManagementTest/UserManagementTest.cs
+++ TBZ_KrakenBracket/TBZ.UserManagementTest/UserManagementTest.cs
@@ -8,25 +8,94 @@ namespace TBZ.UserManagementTest
     public class UserManagementTest
     {
         /// <summary>
-        /// Test method for creating a user as an Admin
+        /// Single creating users as admin
         /// </summary>
+        /// <param name="sysID">System ID</param>
+        /// <param name="password">Password</param>
+        /// <param name="permission">Permission</param>
         [DataTestMethod]
-        [DataRow(100,  "Brian!!!9039", "Admin")]
+        [DataRow(2000,  "Brian!!!9039", "Admin")]
+        [DataRow(619, "89wf[8@negjJH", "Admin")]
         public void SingleCreateUsers_Admin_Pass(int sysID, string password, string permission)
         {
+            // Arrange
             var userManagement = new UserManagement();
             bool result;
+
+            // Act
             try
             {
-                result = userManagement.SingleCreateUsers(sysID,  password, permission);
+                result = userManagement.SingleCreateUsers(sysID, password, permission);
             }
             catch (ArgumentException)
             {
                 result = false;
             }
+            catch (Exception) { result = false; }
+
+            // Assert
             Assert.IsTrue(result);
         }
 
-     
+        /// <summary>
+        /// Bulk creating users with no password check
+        /// </summary>
+        /// <param name="listOfIDs"></param>
+        /// <param name="listOfPasswords"></param>
+        /// <param name="passwordCheck">boolean to enforce password check or not</param>
+        [DataTestMethod]
+        [DataRow(new int[] { 1, 2, 3 }, new string[] {" ", "password" , "123" }, false)]
+        public void BulkCreateUsers_NoPasswordCheck_Pass(int[] listOfIDs, string[] listOfPasswords, bool passwordCheck)
+        {
+            // Arrange
+            var userManagement = new UserManagement();
+            bool[] expected = { true, true, true };
+            bool[] actual;
+
+            // Act
+            actual = userManagement.BulkCreateUsers(listOfIDs, listOfPasswords, passwordCheck);
+            
+            // Actual
+            CollectionAssert.AreEqual(actual, expected);
+        }
+
+        /// <summary>
+        /// Bulk creating users with password check enabled
+        /// </summary>
+        /// <param name="listOfIDs"></param>
+        /// <param name="listOfPasswords"></param>
+        /// <param name="passwordCheck"></param>
+        [DataTestMethod]
+        [DataRow(new int[] { 1, 2, 3 }, new string[] { "BridgesIHaveBurned392!", "JIO389fkqfg4//", "w894uhiM<>" }, true)]
+        public void BulkCreateUsers_PasswordCheck_Pass(int[] listOfIDs, string[] listOfPasswords, bool passwordCheck)
+        {
+            // Arrange
+            var userManagement = new UserManagement();
+            bool[] expected = { true, true, true };
+            bool[] actual;
+
+            // Act
+            actual = userManagement.BulkCreateUsers(listOfIDs, listOfPasswords, passwordCheck);
+
+            // Actual
+            CollectionAssert.AreEqual(actual, expected);
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] { 23, 24, 25}, new string[] { " ", "password", "123" }, true)]
+        public void BulkCreateUsers_PasswordCheck_Fail_InsufficientPasswords(int[] listOfIDs, 
+            string[] listOfPasswords, bool passwordCheck)
+        {
+            // Arrange
+            var userManagement = new UserManagement();
+            bool[] expected = { false, false, false };
+            bool[] actual;
+
+            // Act
+            actual = userManagement.BulkCreateUsers(listOfIDs, listOfPasswords, passwordCheck);
+
+            // Actual
+            CollectionAssert.AreEqual(actual, expected);
+        }
     }
 }
