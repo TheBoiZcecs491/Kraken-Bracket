9713891754819df5c18c42c7ea0e439c9f0fa332
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-02-16 13:13:53 -0800

    test method for bulk deleting users

--- TBZ_KrakenBracket/TBZ.UserManagementTest/UserManagementTest.cs
+++ TBZ_KrakenBracket/TBZ.UserManagementTest/UserManagementTest.cs
@@ -14,13 +14,14 @@ namespace TBZ.UserManagementTest
         /// <param name="password">Password</param>
         /// <param name="permission">Permission</param>
         [DataTestMethod]
+        [DataRow(1, "398h389289hNU(F", "Admin")]
         [DataRow(2000,  "Brian!!!9039", "Admin")]
         [DataRow(619, "89wf[8@negjJH", "Admin")]
         public void SingleCreateUsers_Admin_Pass(int sysID, string password, string permission)
         {
             // Arrange
             var userManagement = new UserManagement();
-            bool result;
+            bool result = false;
 
             // Act
             try
@@ -31,10 +32,7 @@ namespace TBZ.UserManagementTest
             {
                 result = false;
             }
-            catch (Exception) 
-            { 
-                result = false; 
-            }
+            catch (Exception){ } 
 
             // Assert
             Assert.IsTrue(result);
@@ -85,7 +83,7 @@ namespace TBZ.UserManagementTest
             {
                 result = userManagement.SingleCreateUsers(sysID, password, permission);
             }
-            catch (MySql.Data.MySqlClient.MySqlException)
+            catch (ArgumentException)
             {
                 result = true;
             }
@@ -105,7 +103,7 @@ namespace TBZ.UserManagementTest
         /// <param name="listOfPasswords"></param>
         /// <param name="passwordCheck">boolean to enforce password check or not</param>
         [DataTestMethod]
-        [DataRow(new int[] { 1, 2, 3 }, new string[] {" ", "password" , "123" }, false)]
+        [DataRow(new int[] { 100, 200, 300 }, new string[] {" ", "password" , "123" }, false)]
         public void BulkCreateUsers_NoPasswordCheck_Pass(int[] listOfIDs, string[] listOfPasswords, bool passwordCheck)
         {
             // Arrange
@@ -127,7 +125,7 @@ namespace TBZ.UserManagementTest
         /// <param name="listOfPasswords"></param>
         /// <param name="passwordCheck"></param>
         [DataTestMethod]
-        [DataRow(new int[] { 4, 5, 6 }, new string[] { "BridgesIHaveBurned392!", "JIO389fkqfg4//", "w894uhiM<>" }, true)]
+        [DataRow(new int[] { 400, 500, 600 }, new string[] { "BridgesIHaveBurned392!", "JIO389fkqfg4//", "w894uhiM<>" }, true)]
         public void BulkCreateUsers_PasswordCheck_Pass(int[] listOfIDs, string[] listOfPasswords, bool passwordCheck)
         {
             // Arrange
@@ -165,27 +163,75 @@ namespace TBZ.UserManagementTest
             CollectionAssert.AreEqual(actual, expected);
         }
 
-
+        /// <summary>
+        /// Single deleting users
+        /// </summary>
+        /// <param name="sysID"></param>
+        /// <param name="permission"></param>
         [DataTestMethod]
-        [DataRow(1, "Admin")]
+        [DataRow(100, "Admin")]
         public void SingleDeleteUser_Pass(int sysID, string permission)
         {
             // Arrange
             var userManagement = new UserManagement();
-            bool result;
+            bool result = false;
 
+            // Act
             try
             {
                 result = userManagement.SingleDeleteUser(sysID, permission);
             }
-            catch (Exception)
+            catch (ArgumentException)
             {
                 result = false;
             }
+            catch (Exception) { }
 
+            // Assert
             Assert.IsTrue(result);
+        }
 
+        /// <summary>
+        /// Fail test method where attempting to delete a system ID that does not exist
+        /// </summary>
+        /// <param name="sysID"></param>
+        /// <param name="permission"></param>
+        [DataTestMethod]
+        [DataRow(93828, "Admin")]
+        public void SingleDeleteUser_Fail_SystemIDDoesNotExist(int sysID, string permission)
+        {
+            // Arrange
+            var userManagement = new UserManagement();
+            bool result = true;
 
+            // Act
+            try
+            {
+                result = userManagement.SingleDeleteUser(sysID, permission);
+            }
+            catch (ArgumentException) 
+            {
+                result = true;
+            }
+            catch (Exception) { }
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] { 400, 500, 600}, "Admin")]
+        public void BulkDeleteUsers_Pass(int[] listOfIDs, string permission)
+        {
+            var userManagement = new UserManagement();
+            bool[] expected = { true, true, true };
+            bool[] actual;
+
+            // Act
+            actual = userManagement.BulkDeleteUsers(listOfIDs, permission);
+
+            // Actual
+            CollectionAssert.AreEqual(actual, expected);
         }
     }
 }
