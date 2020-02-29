d8ed4fd8dacbad7c496436a34afd2e643daf8501
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-02-29 11:55:40 -0800

    creating users now return list of users, not ids

--- TBZ_KrakenBracket/TBZ.UserManagementService/UserManagement.cs
+++ TBZ_KrakenBracket/TBZ.UserManagementService/UserManagement.cs
@@ -24,28 +24,28 @@ namespace TBZ.UserManagementService
             bool temp = _DataAccessService.CreateUser(user, true);
             if (temp == false)
             {
-                throw new ArgumentException("");
+                throw new ArgumentException("Failed to create user with associated ID");
             }
             return temp;
         }
 
-        public List<List<int>> BulkCreateUsers(List<User> users, bool passwordCheck)
+        public List<List<User>> BulkCreateUsers(List<User> users, bool passwordCheck)
         {
-            List<int> passedIDs = new List<int>();
-            List<int> failedIDs = new List<int>();
+            List<User> passedIDs = new List<User>();
+            List<User> failedIDs = new List<User>();
             foreach(User u in users)
             {
                 bool temp = _DataAccessService.CreateUser(u, passwordCheck);
                 if (temp == true)
                 {
-                    passedIDs.Add(u.SystemID);
+                    passedIDs.Add(u);
                 }
                 else
                 {
-                    failedIDs.Add(u.SystemID);
+                    failedIDs.Add(u);
                 }
             }
-            return new List<List<int>> { passedIDs, failedIDs };
+            return new List<List<User>> { passedIDs, failedIDs };
         }
     }
 }
