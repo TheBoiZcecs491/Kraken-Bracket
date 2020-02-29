c479f8161176cd433e19781d78deccadc5f671ec
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-02-29 11:57:48 -0800

    testing out lists in list / other stuff

--- TBZ_KrakenBracket/TBZ.UserManagementTest/UserManagementTest.cs
+++ TBZ_KrakenBracket/TBZ.UserManagementTest/UserManagementTest.cs
@@ -10,11 +10,11 @@ namespace TBZ.UserManagementTest
     public class UserManagementTest
     {
         [DataTestMethod]
-        [DataRow(1, null, null, null, "84092ujIO@>>>", "User", true, null)]
+        [DataRow(null, null, null, null, "84092ujIO@>>>", "User", true, null)]
         public void SingleCreateUser_Pass(uint sysID, string fName, string lName, string email, string password, string accntType, bool accountStatus, string errMsg)
         {
             // Arrange
-            User user = new User(sysID, fName, lName, email, password, accntType, accountStatus, errMsg);
+            User user = new User(6, fName, lName, email, password, accntType, accountStatus, errMsg);
             var um = new UserManagement();
             bool result;
 
@@ -36,23 +36,24 @@ namespace TBZ.UserManagementTest
         {
             // Arrange
             List<User> users = new List<User>();
+
             User u1 = new User(3, null, null, null, "8*3kmmrMropongig", "User", true, null);
-            
             User u2 = new User(4, null, null, null, "meMEeiaj093QNGEJOW~~~", "User", true, null);
+
             users.Add(u1);
             users.Add(u2);
 
             var um = new UserManagement();
-            List<List<int>> expected = new List<List<int>>()
+            List<List<User>> expected = new List<List<User>>()
             {
-                new List<int>(){3, 4},
-                new List<int>(){ }
+                users,
+                new List<User>() {}
             };
 
 
             // Act
-            List<List<uint>> actual = um.BulkCreateUsers(users, true);
-            //actual.ForEach(Console.WriteLine);
+            List<List<User>> actual = um.BulkCreateUsers(users, true);
+
             //CollectionAssert.AreEqual(expected, actual);
         }
     }
