9cbd80cbf14710e9818ebcbb3c54b5844c7abf3c
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-02-29 11:19:25 -0800

    bulk and single create tests

--- TBZ_KrakenBracket/TBZ.UserManagementTest/UserManagementTest.cs
+++ TBZ_KrakenBracket/TBZ.UserManagementTest/UserManagementTest.cs
@@ -9,25 +9,51 @@ namespace TBZ.UserManagementTest
     [TestClass]
     public class UserManagementTest
     {
+        [DataTestMethod]
+        [DataRow(1, null, null, null, "84092ujIO@>>>", "User", true, null)]
+        public void SingleCreateUser_Pass(uint sysID, string fName, string lName, string email, string password, string accntType, bool accountStatus, string errMsg)
+        {
+            // Arrange
+            User user = new User(sysID, fName, lName, email, password, accntType, accountStatus, errMsg);
+            var um = new UserManagement();
+            bool result;
+
+            // Act
+            try
+            {
+                result = um.SingleCreateUsers(user);
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+
+            // Assert
+            Assert.IsTrue(result);
+        }
         [TestMethod]
         public void BulkCreateUsers_Pass()
         {
+            // Arrange
             List<User> users = new List<User>();
-            User u1 = new User();
-            u1.SystemID = 1;
-            u1.Password = "80ewjf0wNUFEIA@";
-            u1.AccountType = "User";
-
-            User u2 = new User();
-            u2.SystemID = 2;
-            u2.Password = "80ewjf0wNUFEIA@";
-            u2.AccountType = "User";
-
+            User u1 = new User(3, null, null, null, "8*3kmmrMropongig", "User", true, null);
+            
+            User u2 = new User(4, null, null, null, "meMEeiaj093QNGEJOW~~~", "User", true, null);
             users.Add(u1);
             users.Add(u2);
 
             var um = new UserManagement();
-            um.BulkCreateUsers(users, true);
+            List<List<int>> expected = new List<List<int>>()
+            {
+                new List<int>(){3, 4},
+                new List<int>(){ }
+            };
+
+
+            // Act
+            List<List<uint>> actual = um.BulkCreateUsers(users, true);
+            //actual.ForEach(Console.WriteLine);
+            //CollectionAssert.AreEqual(expected, actual);
         }
     }
 }
