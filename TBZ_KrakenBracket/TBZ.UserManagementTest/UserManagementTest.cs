2117d4acdd7a66a783282050864ba52b11a68fa7
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-03-06 15:34:24 -0800

    bulk create test fail insuff password

--- TBZ_KrakenBracket/TBZ.UserManagementTest/UserManagementTest.cs
+++ TBZ_KrakenBracket/TBZ.UserManagementTest/UserManagementTest.cs
@@ -12,7 +12,8 @@ namespace TBZ.UserManagementTest
         [DataTestMethod]
         [DataRow(6u, null, null, null, "84092ujIO@>>>", "User", true, null)]
         [DataRow(12u, null, null, null, "NDIaklnmef*()#!3", "User", true, null)]
-        public void SingleCreateUser_Pass(uint sysID, string fName, string lName, string email, string password, string accntType, bool accountStatus, string errMsg)
+        public void SingleCreateUser_Pass(uint sysID, string fName, string lName, string email,
+            string password, string accntType, bool accountStatus, string errMsg)
         {
             // Arrange
             User user = new User(sysID, fName, lName, email, password, accntType, accountStatus, errMsg);
@@ -119,7 +120,41 @@ namespace TBZ.UserManagementTest
             um.SingleDeleteUser(u1);
             um.SingleDeleteUser(u2);
             // FIXME: error that element 0 on both collections do not match
-            //CollectionAssert.AreEqual(expected, actual);
+            CollectionAssert.AreEqual(expected[0], actual[0]);
+        }
+
+        [TestMethod]
+        public void BulkCreateUsers_PasswordCheck_Fail_InsufficientPasswords()
+        {
+            // Arrange
+            List<User> users = new List<User>();
+
+            User u1 = new User(1, null, null, null, "password", "User", true, null);
+            User u2 = new User(2, null, null, null, "123", "User", true, null);
+            User u3 = new User(3, null, null, null, "", "User", true, null);
+            User u4 = new User(4, null, null, null, null, "User", true, null);
+            User u5 = new User(5, null, null, null, "bad", "User", true, null);
+            User u6 = new User(6, null, null, null, "brian", "User", true, null);
+
+            users.Add(u1);
+            users.Add(u2);
+            users.Add(u3);
+            users.Add(u4);
+            users.Add(u5);
+            users.Add(u6);
+
+            var um = new UserManagement();
+            List<List<User>> expected = new List<List<User>>()
+            {
+                new List<User>(){ }, // Passed ID's
+                users // Failed ID's
+            };
+
+            // Act
+            List<List<User>> actual = um.BulkCreateUsers(users, true);
+
+            // FIXME: error that element 0 on both collections do not match
+            CollectionAssert.AreEqual(expected[1], actual[1]);
         }
 
         [TestMethod]
@@ -128,11 +163,19 @@ namespace TBZ.UserManagementTest
              // Arrange
             List<User> users = new List<User>();
 
-            User u1 = new User(3, null, null, null, "8*3kmmrMropongig", "User", true, null);
-            User u2 = new User(4, null, null, null, "meMEeiaj093QNGEJOW~~~", "User", true, null);
+            User u1 = new User(1, null, null, null, "password", "User", true, null);
+            User u2 = new User(2, null, null, null, "123", "User", true, null);
+            User u3 = new User(3, null, null, null, "", "User", true, null);
+            User u4 = new User(4, null, null, null, null, "User", true, null);
+            User u5 = new User(5, null, null, null, "bad", "User", true, null);
+            User u6 = new User(6, null, null, null, "brian", "User", true, null);
 
             users.Add(u1);
             users.Add(u2);
+            users.Add(u3);
+            users.Add(u4);
+            users.Add(u5);
+            users.Add(u6);
 
             var um = new UserManagement();
             List<List<User>> expected = new List<List<User>>()
@@ -143,16 +186,22 @@ namespace TBZ.UserManagementTest
 
             // Act
             List<List<User>> actual = um.BulkCreateUsers(users, false);
-            um.SingleDeleteUser(u1);
-            um.SingleDeleteUser(u2);
+
             // FIXME: error that element 0 on both collections do not match
-            //CollectionAssert.AreEqual(expected, actual);
+            CollectionAssert.AreEqual(expected[0], actual[0]);
+
+            // Delete users to clean database
+            foreach (User u in users)
+            {
+                um.SingleDeleteUser(u);
+            }
         }
 
         [DataTestMethod]
         [DataRow(6u, null, null, null, "84092ujIO@>>>", "User", true, null)]
         [DataRow(12u, null, null, null, "NDIaklnmef*()#!3", "User", true, null)]
-        public void SingleDeleteUser_Pass(uint sysID, string fName, string lName, string email, string password, string accntType, bool accountStatus, string errMsg)
+        public void SingleDeleteUser_Pass(uint sysID, string fName, string lName, string email,
+            string password, string accntType, bool accountStatus, string errMsg)
         {
             // Arrange
             User user = new User(sysID, fName, lName, email, password, accntType, accountStatus, errMsg);
@@ -180,11 +229,19 @@ namespace TBZ.UserManagementTest
             // Arrange
             List<User> users = new List<User>();
 
-            User u1 = new User(3, null, null, null, "8*3kmmrMropongig", "User", true, null);
-            User u2 = new User(4, null, null, null, "meMEeiaj093QNGEJOW~~~", "User", true, null);
+            User u1 = new User(1, null, null, null, "password", "User", true, null);
+            User u2 = new User(2, null, null, null, "123", "User", true, null);
+            User u3 = new User(3, null, null, null, "", "User", true, null);
+            User u4 = new User(4, null, null, null, null, "User", true, null);
+            User u5 = new User(5, null, null, null, "bad", "User", true, null);
+            User u6 = new User(6, null, null, null, "brian", "User", true, null);
 
             users.Add(u1);
             users.Add(u2);
+            users.Add(u3);
+            users.Add(u4);
+            users.Add(u5);
+            users.Add(u6);
 
             var um = new UserManagement();
 
@@ -200,7 +257,7 @@ namespace TBZ.UserManagementTest
 
             // Assert
             // FIXME: error that element 0 on both collections do not match
-            // CollectionAssert.AreEqual(expected, actual);
+            CollectionAssert.AreEqual(expected[0], actual[0]);
         }
     }
 }
