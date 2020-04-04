991d176cf75208e4eed52174485614d7d032f07e
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-04 12:12:25 -0700

    added logging

---
+++ TBZ_KrakenBracket/TBZ.KrakenBracket.Tests/LoggingTest.cs
@@ -0,0 +1,226 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Threading;
+using TBZ.LoggingService;
+
+namespace TBZ.LoggingTest
+{
+    [TestClass]
+    public class LoggingTest
+    {
+        [TestMethod]
+        public void CreateLoggingInstance_Pass()
+        {
+            //Arrange
+            bool result = true;
+            string path = "output.csv";
+            int tries = 3;
+            //Act
+            try
+            {
+                var l = new Logging(path, tries);
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void LogValidPath_Pass()
+        {
+            //Arrange
+            bool result;
+            string path = "output.csv";
+            string op = "op";
+            string msg = "";
+            string id = "id";
+            int tries = 3;
+            //Act
+            try
+            {
+                var l = new Logging(path, tries);
+                result = l.Log(op, msg, id);
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void LogInvalidPathType_Fail()
+        {
+            //Arrange
+            bool result;
+            string path = "output.txt";
+            string op = "op";
+            string msg = "";
+            string id = "id";
+            int tries = 3;
+            //Act
+            try
+            {
+                var l = new Logging(path, tries);
+                result = l.Log(op, msg, id);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                result = false;
+            }
+            //Assert
+            Assert.IsTrue(!result);
+        }
+
+        [TestMethod]
+        public void LogInvalidTries_Fail()
+        {
+            //Arrange
+            bool result;
+            string path = "output.csv";
+            string op = "op";
+            string msg = "";
+            string id = "id";
+            int tries = 0;
+            //Act
+            try
+            {
+                var l = new Logging(path, tries);
+                result = l.Log(op, msg, id);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                result = false;
+            }
+            //Assert
+            Assert.IsTrue(!result);
+        }
+
+        [TestMethod]
+        public void LogInvalidOperation_Fail()
+        {
+            //Arrange
+            bool result;
+            string path = "output.csv";
+            string op = "";
+            string msg = "";
+            string id = "id";
+            int tries = 3;
+            //Act
+            try
+            {
+                var l = new Logging(path, tries);
+                result = l.Log(op, msg, id);
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            //Assert
+            Assert.IsTrue(!result);
+        }
+
+        [TestMethod]
+        public void LogInvalidID_Fail()
+        {
+            //Arrange
+            bool result;
+            string path = "output.csv";
+            string op = "op";
+            string msg = "";
+            string id = "";
+            int tries = 3;
+            //Act
+            try
+            {
+                var l = new Logging(path, tries);
+                result = l.Log(op, msg, id);
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            //Assert
+            Assert.IsTrue(!result);
+        }
+
+        [TestMethod]
+        public void LogToCSV_Pass()
+        {
+            //Arrange
+            bool result;
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string path = Path.Combine(docPath, "logs.csv");
+            string op = "Registration";
+            string msg = "Data Store Error";
+            string id = "0001";
+            int tries = 3;
+            //Act
+            try
+            {
+                var l = new Logging(path, tries);
+                result = l.Log(op, msg, id);
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Log50Times_Pass()
+        {
+            //Arrange
+            bool result = false;
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string path = Path.Combine(docPath, "logs.csv");
+            string op = "Authorization";
+            string msg = "Invalid Access Error";
+            int tries = 3;
+            //Act
+            Logging l = null;
+            try
+            {
+                l = new Logging(path, tries);
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            for (int i = 0; i < 50; i++)
+            {
+                if (i == 17)
+                {
+                    op = "Authentication";
+                    msg = "Invalid Claim Error";
+                }
+                if (i == 32)
+                {
+                    op = "Registration";
+                    msg = "Data Store Error";
+                }
+                string id = i.ToString();
+                try
+                {
+                    result = l.Log(op, msg, id);
+                    Thread.Sleep(60);
+                }
+                catch (Exception)
+                {
+                    result = false;
+                }
+            }
+            //Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
