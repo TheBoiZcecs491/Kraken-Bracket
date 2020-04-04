46cfdd0dffa3d3e9cd0ee7cfd026d43c8c7c8e62
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-04 11:51:38 -0700

    added authorization

---
+++ TBZ_KrakenBracket/TBZ.KrakenBracket.Managers/Authorization.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using TBZ.DatabaseAccess;
+
+namespace TBZ.AuthorizationManager
+{
+    public class Authorization
+    {
+        public bool UserPermission(string email, string action, bool isLoggedIn)
+        {
+            Regex rgx = new Regex(action);
+            var dataAccess = new DataAccess();
+            bool permission = false;
+            string claim = (string)dataAccess.DSGetClaim(email);
+            // If user is non-registered
+            if ((string.IsNullOrEmpty(email)) && (isLoggedIn == false) && claim.Contains(action))
+            {
+                permission = true;
+            }
+            // Access to users who are registered and non-registered (not logged in)
+            else if ((isLoggedIn) && (claim.Contains(action)))
+            {
+                permission = true;
+            }
+            // If action is not found or claim does not match, display restriction message
+            else if ((!isLoggedIn))
+            {
+                throw new ArgumentException("User is not logged in\n");
+            }
+            else
+            {
+                throw new ArgumentException("User is not authorized to perform: " + action);
+            }
+            return permission;
+        }
+    }
+}
