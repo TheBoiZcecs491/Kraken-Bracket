ea0cf3096f2457a654d35c87d8d39132a8ac5418
Kevin Phan <keviinphan@gmail.com>
2020-04-11 18:12:06 -0700

    Started on methods

---
+++ TBZ_KrakenBracket/TBZ.KrakenBracket.Managers/TournamentBracketManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TBZ.AuthorizationManager;
+using TBZ.DatabaseAccess;
+using TBZ.KrakenBracket.Services;
+
+namespace TBZ.KrakenBracket.Managers
+{
+    class TournamentBracketManager
+    {
+        private readonly TournamentBracketService _TournamentBracketService = new TournamentBracketService();
+        private readonly DataAccess TournamentDA = new DataAccess();
+        public bool TournamentPermission(string email, string action, bool isLoggedIn)
+        {
+            Authorization AuthZ = new Authorization();
+            return AuthZ.UserPermission(email, action, isLoggedIn);
+        }
+    }
+}
---
+++ TBZ_KrakenBracket/TBZ.KrakenBracket.Services/TournamentBracketService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TBZ.DatabaseAccess;
+
+namespace TBZ.KrakenBracket.Services
+{
+    public class TournamentBracketService
+    {
+        public int TournamentClaim(DataAccess _TournamentDA, string email, string claim, bool isLoggedIn)
+        {
+            return 0;
+        }
+        public bool GetBracketID(string email)
+        {
+            bool result = true;
+            return result;
+        }
+    }
+}
