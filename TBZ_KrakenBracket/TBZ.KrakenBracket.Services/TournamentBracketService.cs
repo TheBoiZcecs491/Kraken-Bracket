db7749d48698a258435a154a560a39c95d02f1d7
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-12 13:04:07 -0700

    implemented check status code in service

--- TBZ_KrakenBracket/TBZ.KrakenBracket.Services/TournamentBracketService.cs
+++ TBZ_KrakenBracket/TBZ.KrakenBracket.Services/TournamentBracketService.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TBZ.KrakenBracket.DatabaseAccess;
+using TBZ.KrakenBracket.DataHelpers;
 
 namespace TBZ.KrakenBracket.Services
 {
     public class TournamentBracketService
     {
+        private readonly TournamentBracketDataAccess _tournamentBracketDataAccess;
+        public TournamentBracketService(TournamentBracketDataAccess tournamentBracketDataAccess)
+        {
+            _tournamentBracketDataAccess = tournamentBracketDataAccess;
+        }
+        public int CheckBracketStatusCode(BracketInfo bracket)
+        {
+            
+            var result = _tournamentBracketDataAccess.GetBracketStatus(bracket);
+            return result;
+        }
     }
 }
