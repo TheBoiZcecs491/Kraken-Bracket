552105fcede63dcde81bc98f87a1b314f0a7f07b
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-12 12:39:33 -0700

    get status code on bracket manager

--- TBZ_KrakenBracket/TBZ.KrakenBracket.Managers/TournamentBracketManager.cs
+++ TBZ_KrakenBracket/TBZ.KrakenBracket.Managers/TournamentBracketManager.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TBZ.KrakenBracket.DataHelpers;
+using TBZ.KrakenBracket.Services;
 
 namespace TBZ.KrakenBracket.Managers
 {
-    class TournamentBracketManager
+    public class TournamentBracketManager
     {
+        private readonly TournamentBracketService _tournamentBracketService;
+        public TournamentBracketManager(TournamentBracketService tournamentBracketService)
+        {
+            _tournamentBracketService = tournamentBracketService;
+        }
+        public BracketInfo GetStatusCode(int bracketID)
+        {
+            
+        }
     }
 }
