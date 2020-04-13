05cafc4bd1469856b4350b75881e1922275f4c80
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-13 16:40:41 -0700

    added bracket list view and bracket model

---
+++ krakenbracket-frontend/src/views/BracketList.view.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Bracket Listings</h1>
+        <Bracket v-for="bracket in brackets" :key="bracket.id" :event="bracket"/>
+    </div>
+</template>
+
+<script>
+import Bracket from '@/components/Bracket.model.vue'
+import axios from 'axios'
+export default {
+    components:{
+        Bracket
+    },
+    data(){
+        return {
+            brackets: []
+        }
+    },
+    created(){
+        axios
+        .get('http://localhost:3000/brackets')
+        .then(response => {
+            this.brackets = response.data
+        })
+        .catch(error =>{
+            console.log("There was an error: " + error);
+        })
+    }
+}
+</script>
