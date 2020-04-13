e53aa25bc577c8034c29527fb6f7dc6ce56f766e
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-12 20:38:22 -0700

    added test cases

--- TBZ_KrakenBracket/TBZ.KrakenBracket.Tests/TournamentBracketTest.cs
+++ TBZ_KrakenBracket/TBZ.KrakenBracket.Tests/TournamentBracketTest.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TBZ.KrakenBracket.DataHelpers;
 using TBZ.KrakenBracket.Managers;
 
 namespace TBZ.KrakenBracket.Tests
@@ -9,13 +10,26 @@ namespace TBZ.KrakenBracket.Tests
     [TestClass]
     public class TournamentBracketTest
     {
-        private readonly TournamentBracketManager _tournamentBracketManager;
-        public TournamentBracketTest(TournamentBracketManager tournamentBracketManager)
-        {
-            _tournamentBracketManager = tournamentBracketManager;
-        }
-        [TestClass]
+        private readonly TournamentBracketManager _tournamentBracketManager = new TournamentBracketManager();
         
+        [TestMethod]
+        public void GetBracketStatusCode_Pass()
+        {
+            BracketInfo bracketInfo = new BracketInfo(1, "Test Name", 1, 127, "Mortal Kombat 11", "Xbox One", 
+                "No rules atm", new DateTime(2020, 5, 3), new DateTime(2020, 5, 3), 0);
+            var expected = 1;
+            var actual = _tournamentBracketManager.GetBracketStatusCode(bracketInfo.BracketID);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void GetNumberOfCompetitors_Pass()
+        {
+            BracketInfo bracketInfo = new BracketInfo(1, "Test Name", 1, 127, "Mortal Kombat 11", "Xbox One",
+                "No rules atm", new DateTime(2020, 5, 3), new DateTime(2020, 5, 3), 0);
+            var expected = bracketInfo.PlayerCount;
+            var actual = _tournamentBracketManager.GetNumberOfCompetitors(bracketInfo.BracketID);
+            Assert.AreEqual(expected, actual);
+        }
         
     }
 }
