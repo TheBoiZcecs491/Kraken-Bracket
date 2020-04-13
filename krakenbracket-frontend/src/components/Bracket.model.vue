fca2c9664291ac9fc255451a92f0a429c296e5c9
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-13 16:57:14 -0700

    removed hard coded values

--- krakenbracket-frontend/src/components/Bracket.model.vue
+++ krakenbracket-frontend/src/components/Bracket.model.vue
@@ -1,15 +1,13 @@
 <template>
     <div>
-        <h1>{{title}}</h1>
+        
     </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            title: "bracket 1"
-        }
+    props: {
+        bracket: Object
     }
 }
 </script>
