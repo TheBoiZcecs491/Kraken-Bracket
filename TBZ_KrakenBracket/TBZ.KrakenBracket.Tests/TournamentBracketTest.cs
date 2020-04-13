abeea53e62e30a55a6c3ae43c97370325c654ba2
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-12 19:50:15 -0700

    tournament bracket test class

---
+++ TBZ_KrakenBracket/TBZ.KrakenBracket.Tests/TournamentBracketTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TBZ.KrakenBracket.Managers;
+
+namespace TBZ.KrakenBracket.Tests
+{
+    [TestClass]
+    public class TournamentBracketTest
+    {
+        private readonly TournamentBracketManager _tournamentBracketManager;
+        public TournamentBracketTest(TournamentBracketManager tournamentBracketManager)
+        {
+            _tournamentBracketManager = tournamentBracketManager;
+        }
+        [TestClass]
+        
+        
+    }
+}
