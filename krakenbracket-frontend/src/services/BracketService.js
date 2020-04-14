517d039b7d2ee2e2338078966783317b0dadd247
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-13 21:42:05 -0700

    changes to router

--- krakenbracket-frontend/src/services/BracketService.js
+++ krakenbracket-frontend/src/services/BracketService.js
@@ -1,17 +1,16 @@
 import axios from 'axios'
-
-const apiClient = axios.create({
-    baseURL: 'http://localhost:3000',
-    withCredentials: false,
-    headers: {
+    
+    const apiClient = axios.create({  
+      baseURL: `http://localhost:3000`,
+      withCredentials: false, // This is the default
+      headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
-    }
-})
-
-export default {
-    getBrackets() {
-      return apiClient.get('/brackets')
-    }
+      }
+    })
     
-  }
+    export default {
+      getBrackets() {
+        return apiClient.get('/brackets')
+      }
+    }
