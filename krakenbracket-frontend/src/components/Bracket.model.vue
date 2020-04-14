f128b08671b348a63ec6b44452d2cad393edfaee
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-13 21:40:24 -0700

    testing out some attributes

--- krakenbracket-frontend/src/components/Bracket.model.vue
+++ krakenbracket-frontend/src/components/Bracket.model.vue
@@ -1,10 +1,16 @@
 <template>
     <div>
-        
+        <h2>Bracket Name: {{bracket.bracket_name}}</h2>
+        <h4>Bracket ID: {{bracket.id}}</h4>
+        <h4>Game Played: {{bracket.game_played}}</h4>
+        <h3>Number of competitors: {{bracket.number_player}}</h3>
+        <p>------------------------</p>
     </div>
+        
 </template>
 
 <script>
+
 export default {
     props: {
         bracket: Object
