a23a64d88fcec66c84f45c1e621e1fafbda2bcc7
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-16 17:47:04 -0700

    feat: added styles to bracket model

--- krakenbracket-frontend/src/components/Bracket.model.vue
+++ krakenbracket-frontend/src/components/Bracket.model.vue
@@ -1,11 +1,10 @@
 <template>
-    <router-link :to="{name : 'bracket-view', params: {id: bracket.id}}">
-    <div>
-        <h2>Bracket Name: {{bracket.bracket_name}}</h2>
-        <h4>Bracket ID: {{bracket.id}}</h4>
-        <h4>Game Played: {{bracket.game_played}}</h4>
-        <h3>Number of competitors: {{bracket.number_player}}</h3>
-        <p>------------------------</p>
+    <router-link class="bracket-link" :to="{name : 'bracket-view', params: {id: bracket.id}}">
+    <div class="bracket-card">
+        <h2>{{bracket.bracket_name}}</h2>
+        <h3>Start Date: {{bracket.start_date}}</h3>
+        <h3>End Date: {{bracket.end_date}}</h3>
+        <h4>Number of players: {{bracket.number_player}}</h4>
     </div>
     </router-link>
 </template>
@@ -18,3 +17,24 @@ export default {
     }
 }
 </script>
+
+<style>
+.bracket-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.bracket-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.bracket-card > .title {
+  margin: 0;
+}
+.bracket-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
+}
+</style>
