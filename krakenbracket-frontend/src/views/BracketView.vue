149dd5863ac65e8f923648712a7578894762f23e
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-16 17:47:52 -0700

    feat: displayed more attributes for bracket view

--- krakenbracket-frontend/src/views/BracketView.vue
+++ krakenbracket-frontend/src/views/BracketView.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
-        <h1>Showing bracket ID #{{id}}</h1>
-        <h2>Bracket Name: {{bracket.bracket_name}}</h2>
-        <h4>Bracket ID: {{bracket.id}}</h4>
-        <h4>Game Played: {{bracket.game_played}}</h4>
-        <h3>Number of competitors: {{bracket.number_player}}</h3>
-        <p>------------------------</p>
+        <h1 class="title">{{bracket.bracket_name}}</h1>
+        <h3>Start Date: {{bracket.start_date}}</h3>
+        <h3>End Date: {{bracket.end_date}}</h3>
+        <h4>Number of players: {{bracket.number_player}}</h4>
+        <h4>Game: {{bracket.game_played}}</h4>
+        <h4>Gaming platform: {{bracket.gaming_platform}}</h4>
+        <div>
+            <h4>Rules:</h4>
+            <p>{{bracket.rules}}</p>
+        </div>
+        <button>Register!</button>
     </div>
 </template>
 <script>
@@ -28,3 +33,10 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.title{
+    font-size: 100px;
+    font-weight: 800;
+}
+</style>
