9db4a5f79ca1220ea6e567646e6ed9db5e303b92
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-18 13:07:01 -0700

    feat(tests): new cy.url().should() method

--- krakenbracket-frontend/tests/e2e/specs/test.js
+++ krakenbracket-frontend/tests/e2e/specs/test.js
@@ -3,29 +3,31 @@
 describe("My First Test", () => {
   it("Visits the app root url", () => {
     cy.visit("http://localhost:8080/#/");
-    cy.contains("h1", "Welcome to Your Vue.js App");
+    cy.contains("h1", "Welcome to Kraken Bracket");
   });
 });
 
 describe("Viewing specific brackets", () => {
   it("Visits the app root url", () => {
     cy.visit("http://localhost:8080/#/");
-    cy.contains("h1", "Welcome to Your Vue.js App");
+    cy.contains("h1", "Welcome to Kraken Bracket");
   });
   it("Clicks on the Bracket List router link", () => {
-    cy.pause();
+    //cy.pause();
     cy.contains("Bracket List").click();
   });
   it("Makes sure you are on the bracket listings", () => {
-    cy.pause();
+    //cy.pause();
     cy.contains("h1", "Bracket Listings");
+    cy.url().should('include', '/bracket-list')
   });
   it("Clicks on a bracket", () => {
-    cy.pause();
+    //cy.pause();
     cy.get(".bracket-card:first").click();
   });
   it("Makes sure you are on a specific bracket page", () => {
     cy.get("#title");
     cy.contains("button", "Register!");
+    cy.url().should("include", "bracket-view");
   });
 });
