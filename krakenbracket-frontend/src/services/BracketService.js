eae0da09ea8616876489493453fcdba727ce8900
Kevin Phan <k3vinphan@hotmail.com>
2020-04-18 16:44:43 -0700

    Running into issues2

--- krakenbracket-frontend/src/services/BracketService.js
+++ krakenbracket-frontend/src/services/BracketService.js
@@ -1,24 +1,4 @@
-<<<<<<< HEAD
-import axios from "axios";
 
-const apiClient = axios.create({
-  baseURL: `https://localhost:44352/api`,
-  withCredentials: false, // This is the default
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
-export default {
-  getBrackets() {
-    return apiClient.get("/brackets");
-  },
-  getBracketByID(bracketID) {
-    return apiClient.get("/brackets/" + bracketID);
-  }
-};
-=======
 import axios from 'axios'
     
     const apiClient = axios.create({  
@@ -35,4 +15,3 @@ import axios from 'axios'
         return apiClient.get('/brackets')
       }
     }
->>>>>>> parent of 1f5a6a7... Bracket Info Displayed (via URL)
