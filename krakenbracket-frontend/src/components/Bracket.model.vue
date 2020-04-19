b821f6c5fa0e0aa4608ad54011e84abade5a9c3f
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-18 21:18:46 -0700

    added button to route to BracketView

--- krakenbracket-frontend/src/components/Bracket.model.vue
+++ krakenbracket-frontend/src/components/Bracket.model.vue
@@ -1,15 +1,16 @@
 <template>
-  <router-link
-    class="bracket-link"
-    :to="{ name: 'bracket-view', params: { id: bracket.bracketID } }"
-  >
-    <div class="bracket-card">
-      <h2>{{ bracket.bracketName }}</h2>
-      <h3>Start Date: {{ bracket.startDate }}</h3>
-      <h3>End Date: {{ bracket.startDate }}</h3>
-      <h4>Number of players: {{ bracket.playerCount }}</h4>
-    </div>
-  </router-link>
+  <div class="bracket-card">
+    <h2>{{ bracket.bracketName }}</h2>
+    <h3>Start Date: {{ bracket.startDate }}</h3>
+    <h3>End Date: {{ bracket.startDate }}</h3>
+    <h4>Number of players: {{ bracket.playerCount }}</h4>
+    <router-link
+      class="bracket-link"
+      :to="{ name: 'bracket-view', params: { id: bracket.bracketID } }"
+    >
+      <v-btn class="primary" color="#2196F3">View Bracket</v-btn>
+    </router-link>
+  </div>
 </template>
 
 <script>
@@ -25,11 +26,6 @@ export default {
   padding: 20px;
   margin-bottom: 24px;
   transition: all 0.2s linear;
-  cursor: pointer;
-}
-.bracket-card:hover {
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
 }
 .bracket-card > .title {
   margin: 0;
