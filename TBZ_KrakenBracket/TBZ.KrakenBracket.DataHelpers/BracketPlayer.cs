220c8386a9504f95319df8f8a260fe4157aca4ef
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-19 12:43:35 -0700

    fix: moved classes into their own files

---
+++ TBZ_KrakenBracket/TBZ.KrakenBracket.DataHelpers/BracketPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TBZ.KrakenBracket.DataHelpers
+{
+    public class BracketPlayer
+    {
+        public int BracketID { get; set; }
+        public int HashedUserID { get; set; }
+        public int RoleID { get; set; }
+        public int Placement { get; set; }
+        public int Score { get; set; }
+        public BracketPlayer() { }
+        public BracketPlayer(int bracketID, int hashedUserID, int roleID, int placement, int score)
+        {
+            BracketID = bracketID;
+            HashedUserID = hashedUserID;
+            RoleID = roleID;
+            Placement = placement;
+            Score = score;
+        }
+    }
+}
---
+++ TBZ_KrakenBracket/TBZ.KrakenBracket.DataHelpers/Gamer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TBZ.KrakenBracket.DataHelpers
+{
+    public class Gamer
+    {
+        public int HashedUserID { get; set; }
+        public string GamerTag { get; set; }
+        public int GamerTagID { get; set; }
+        public int TeamID { get; set; }
+        public Gamer() { }
+        public Gamer(int hashedUserID, string gamerTag, int gamerTagID, int teamID)
+        {
+            HashedUserID = hashedUserID;
+            GamerTag = gamerTag;
+            GamerTagID = gamerTagID;
+            TeamID = teamID;
+        }
+    }
+}
