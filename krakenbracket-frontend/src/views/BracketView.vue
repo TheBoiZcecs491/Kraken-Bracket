a6f09844acbd451b85c2341af68be576d8f9e1bf
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-18 20:49:35 -0700

    feat: router link to bracket registration form

--- krakenbracket-frontend/src/views/BracketView.vue
+++ krakenbracket-frontend/src/views/BracketView.vue
@@ -1,18 +1,20 @@
 <template>
-<v-app>
-  <div>
-    <h1 id="title">{{ bracket.bracketName }}</h1>
-    <h3>Start Date: {{ bracket.startDate }}</h3>
-    <h3>End Date: {{ bracket.endDate }}</h3>
-    <h4>Number of players: {{ bracket.playerCount }}</h4>
-    <h4>Game: {{ bracket.gamePlayed }}</h4>
-    <h4>Gaming platform: {{ bracket.gamingPlatform }}</h4>
+  <v-app>
     <div>
-      <h4>Rules:</h4>
-      <p>{{ bracket.rules }}</p>
+      <h1 id="title">{{ bracket.bracketName }}</h1>
+      <h3>Start Date: {{ bracket.startDate }}</h3>
+      <h3>End Date: {{ bracket.endDate }}</h3>
+      <h4>Number of players: {{ bracket.playerCount }}</h4>
+      <h4>Game: {{ bracket.gamePlayed }}</h4>
+      <h4>Gaming platform: {{ bracket.gamingPlatform }}</h4>
+      <div>
+        <h4>Rules:</h4>
+        <p>{{ bracket.rules }}</p>
+      </div>
+      <router-link :to="{ name: 'bracket-registration', params: { id: bracket.bracketID } }">
+        <v-btn type="submit">Register!</v-btn>
+      </router-link>
     </div>
-    <v-btn>Register!</v-btn>
-  </div>
   </v-app>
 </template>
 
