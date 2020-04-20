ce854becf415c44ba9992c7860a60c469811f885
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-19 19:06:54 -0700

    feat: temporary login view
    The only purpose of the view is to change the user state of
    'isLoggedIn'. This will change in the future

---
+++ krakenbracket-frontend/src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app>
+        <div>
+            <v-btn @click="logInUser" color="success">Log In</v-btn>
+        </div>
+    </v-app>
+</template>
+
+<script>
+export default {
+    methods: {
+        logInUser(){
+            this.$store.commit('CHANGE_LOGGED_IN_STATUS')
+        }
+    }
+}
+</script>
