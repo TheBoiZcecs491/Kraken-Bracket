f98ef480e44fd838a256e124674eb0fdff1026d4
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-20 19:26:21 -0700

    feat: styles; notfication when bracket is full

--- krakenbracket-frontend/src/components/Bracket.model.vue
+++ krakenbracket-frontend/src/components/Bracket.model.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="bracket-card">
-    <h2>{{ bracket.bracketName }}</h2>
+    <h2 class="bracket-name">{{ bracket.bracketName }}</h2>
     <h3>Start Date: {{ bracket.startDate }}</h3>
     <h3>End Date: {{ bracket.startDate }}</h3>
-    <h4>Number of players: {{ bracket.playerCount }}</h4>
+    <h4>
+      Number of players: {{ bracket.playerCount }}
+      <span v-show="bracket.playerCount === 128">(MAX)</span>
+    </h4>
+    <br />
     <router-link
       class="bracket-link"
       :to="{ name: 'bracket-view', params: { id: bracket.bracketID } }"
@@ -24,15 +28,18 @@ export default {
 <style>
 .bracket-card {
   padding: 20px;
+  padding-top: 30px;
   margin-bottom: 24px;
   transition: all 0.2s linear;
-}
-.bracket-card > .title {
-  margin: 0;
 }
 .bracket-link {
   color: black;
   text-decoration: none;
   font-weight: 100;
+}
+.bracket-name {
+  padding-bottom: 10px;
+  font-style: italic;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
 }
 </style>
