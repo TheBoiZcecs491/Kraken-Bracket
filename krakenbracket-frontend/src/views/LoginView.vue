d5a39f47ffde764dd347ddf1363f18034a32526d
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-20 19:30:31 -0700

    feat: login view now supports logging out

--- krakenbracket-frontend/src/views/LoginView.vue
+++ krakenbracket-frontend/src/views/LoginView.vue
@@ -1,17 +1,23 @@
 <template>
-    <v-app>
-        <div>
-            <v-btn @click="logInUser" color="success">Log In</v-btn>
-        </div>
-    </v-app>
+  <v-app>
+    <div>
+      <v-btn
+        v-if="$store.state.user.isLoggedIn === false"
+        @click="logInUser"
+        color="success"
+        >Log In</v-btn
+      >
+      <v-btn v-else @click="logInUser" color="red text--lighten">Log out</v-btn>
+    </div>
+  </v-app>
 </template>
 
 <script>
 export default {
-    methods: {
-        logInUser(){
-            this.$store.commit('CHANGE_LOGGED_IN_STATUS')
-        }
+  methods: {
+    logInUser() {
+      this.$store.commit("CHANGE_LOGGED_IN_STATUS");
     }
-}
+  }
+};
 </script>
