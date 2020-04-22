98f763fcda6f86d942133a05e939670614403fd3
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-22 15:35:48 -0700

    fix: HashedUserID is now string

--- TBZ_KrakenBracket/TBZ.KrakenBracket.DataHelpers/BracketPlayer.cs
+++ TBZ_KrakenBracket/TBZ.KrakenBracket.DataHelpers/BracketPlayer.cs
@@ -7,12 +7,12 @@ namespace TBZ.KrakenBracket.DataHelpers
     public class BracketPlayer
     {
         public int BracketID { get; set; }
-        public int HashedUserID { get; set; }
+        public string HashedUserID { get; set; }
         public int RoleID { get; set; }
         public int Placement { get; set; }
         public int Score { get; set; }
         public BracketPlayer() { }
-        public BracketPlayer(int bracketID, int hashedUserID, int roleID, int placement, int score)
+        public BracketPlayer(int bracketID, string hashedUserID, int roleID, int placement, int score)
         {
             BracketID = bracketID;
             HashedUserID = hashedUserID;
