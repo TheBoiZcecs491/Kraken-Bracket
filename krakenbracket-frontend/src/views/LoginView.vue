4f40b8f5843ddc071fda44b2dd21c61752329257
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-22 16:32:29 -0700

    login view now accepts email and password

--- krakenbracket-frontend/src/views/LoginView.vue
+++ krakenbracket-frontend/src/views/LoginView.vue
@@ -1,22 +1,60 @@
 <template>
   <v-app>
     <div>
-      <v-btn
+      <v-container>
+        <form @submit.prevent="login">
+          <v-row>
+            <v-col cols="4"></v-col>
+            <v-col cols="12" sm="4">
+              <v-text-field
+                label="Email"
+                type="email"
+                placeholder="john@foomail.com"
+                v-model="email"
+                required
+              ></v-text-field>
+              <v-text-field
+                label="Password"
+                type="password"
+                v-model="password"
+                required
+              ></v-text-field>
+              <v-btn @click="login">Login</v-btn>
+            </v-col>
+            <v-col cols="4"></v-col>
+          </v-row>
+        </form>
+      </v-container>
+      <!-- <v-btn
         v-if="$store.state.user.isLoggedIn === false"
         @click="logInUser"
         color="success"
         >Log In</v-btn
       >
-      <v-btn v-else @click="logInUser" color="red text--lighten">Log out</v-btn>
+      <v-btn v-else @click="logInUser" color="red text--lighten">Log out</v-btn> -->
     </div>
   </v-app>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
   methods: {
-    logInUser() {
-      this.$store.commit("CHANGE_LOGGED_IN_STATUS");
+    login() {
+      // this.$store.commit("CHANGE_LOGGED_IN_STATUS");
+      this.$store
+        .dispatch("login", {
+          email: this.email,
+          password: this.password
+        })
+        .then(() => {
+          this.$router.push({ name: "Home" });
+        });
     }
   }
 };
