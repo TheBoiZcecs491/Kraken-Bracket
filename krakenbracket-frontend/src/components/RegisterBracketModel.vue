3834d11ef25bca7078ef1d94651c4b2a13444c16
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-26 15:43:00 -0700

    new file: BracketRegisterModel
    This component is used to eliminate code congestion when registering for
    bracket

---
+++ krakenbracket-frontend/src/components/RegisterBracketModel.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app>
+    <div style="text-align: left">
+          <!-- <RegisterBracketModel :key="bracket.id" :bracket="bracket" /> -->
+          <router-link
+            :to="{
+              name: 'bracket-registration',
+              params: { id: bracket.bracketID }
+            }"
+            class="register-btn"
+          >
+            <v-btn
+              v-show="bracket.statusCode === 0 && bracket.playerCount < 128"
+              type="submit"
+              >Register!</v-btn
+            >
+          </router-link>
+          <div v-if="bracket.statusCode === 1">
+            <p>
+              <strong>NOTE:</strong> Registration is disabled; bracket has
+              already completed
+            </p>
+          </div>
+          <div v-else-if="bracket.statusCode === 2">
+            <p>
+              <strong>NOTE:</strong> Registration is disabled; bracket is in
+              progress
+            </p>
+          </div>
+          <v-btn
+            v-show="bracket.statusCode !== 0 || bracket.playerCount === 128"
+            disabled
+            >Register!</v-btn
+          >
+        </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    bracket: Object
+  }
+};
+</script>
+
+<style>
+.register-btn {
+  text-decoration: none;
+}
+</style>
