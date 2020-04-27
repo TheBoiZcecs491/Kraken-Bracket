317f9e3f648bed7823469820b1d7d90a704f67be
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-27 13:54:45 -0700

    fix: AuthN overhaul
    This is only a temporary fix. I plan on using JWT down the line

---
+++ TBZ_KrakenBracket/ClientApp/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TBZ.KrakenBracket.DataHelpers;
+using TBZ.KrakenBracket.Managers;
+
+namespace ClientApp.Controllers
+{
+    [Route("api/login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly AuthenticationManager _authenticationManager;
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            return Ok(_authenticationManager.Login(email, password));
+        }
+    }
+}
