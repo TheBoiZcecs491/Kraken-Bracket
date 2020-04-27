894c9335c1cb7a8909edb5ab8932716fba065998
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-27 16:49:35 -0700

    new file: GamerDataAccess.cs

---
+++ TBZ_KrakenBracket/TBZ.KrakenBracket.DatabaseAccess/GamerDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TBZ.DatabaseQueryService;
+using TBZ.KrakenBracket.DataHelpers;
+
+namespace TBZ.KrakenBracket.DatabaseAccess
+{
+    public class GamerDataAccess
+    {
+        public Gamer GetGamerInfo(Gamer gamer)
+        {
+            DatabaseQuery databaseQuery = new DatabaseQuery();
+            return databaseQuery.GetGamerInfo(gamer);
+        }
+    }
+}
