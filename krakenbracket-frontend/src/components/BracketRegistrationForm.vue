b2f8b9402e07a6526eaccc533e2a5dca0d340bac
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-04-29 15:23:54 -0700

    fix: encapsulated buttons in columns

--- krakenbracket-frontend/src/components/BracketRegistrationForm.vue
+++ krakenbracket-frontend/src/components/BracketRegistrationForm.vue
@@ -39,22 +39,21 @@
                 :rules="gamerTagIDRules"
                 required
               ></v-text-field>
+              <v-row>
+                <v-col cols="12" lg="4"></v-col>
+                <v-col cols="12" lg="4">
+                  <v-btn class="mr-4" v-show="!formValidity" :disabled="!formValidity">Register!</v-btn>
+                </v-col>
+                <v-col cols="12" lg="4">
+                  <v-btn class="mr-4" color="error" @click="resetForm">Reset Form</v-btn>
+                </v-col>
+                <!-- <v-col cols="12" lg="4">
+                  <v-btn class="mr-4" color="warning" @click="validateForm">Validate Form</v-btn>
+                </v-col> -->
+              </v-row>
             </v-col>
           <v-col cols="4"></v-col>
           </v-row>
-              <router-link v-if="formValidity"
-                :to="{
-                  name: 'bracket-view',
-                  params: { id: bracket.bracketID }
-                }"
-              >
-                <v-btn class="mr-4" @click="formSubmit" type="submit" color="primary"
-                   >Register!</v-btn
-                >
-              </router-link>
-              <v-btn class="mr-4" v-show="!formValidity" :disabled="!formValidity">Register!</v-btn>
-              <v-btn class="mr-4" color="error" @click="resetForm">Reset Form</v-btn>
-              <v-btn class="mr-4" color="warning" @click="validateForm">Validate Form</v-btn>
         </v-container>
       </v-form>
     </div>
