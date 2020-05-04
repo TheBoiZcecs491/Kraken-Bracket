f008d96090134f7d79c572f739ddd6498b710d62
Kevin Simon <rocpadge@gmail.com>
2020-05-03 18:00:01 -0700

    removed an unused view I created.

--- krakenbracket-frontend/src/views/RegistrationSuccess.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h1>Registration Successfull</h1>
-    <p>
-      You may now <router-link to="/login">Login</router-link> into your account.
-    </p>
-  </div>
-</template>
