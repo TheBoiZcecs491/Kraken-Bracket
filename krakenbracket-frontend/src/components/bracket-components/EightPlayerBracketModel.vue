d441f9f17f0a733bbb2ca9408e210d766f3df982
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-05-08 19:11:10 -0700

    feat: eight player bracket model

---
+++ krakenbracket-frontend/src/components/bracket-components/EightPlayerBracketModel.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <bracket :rounds="rounds">
+            <template #player="{ player }">
+                {{ player.name }}
+            </template>
+        </bracket>
+        <!-- <p v-for="competitor in competitors" 
+        :key="competitor.bracketID" 
+        :competitor="competitor">{{competitor.gamerTag}}</p> -->
+        <p>{{competitors}}</p>
+        <button @click="test">Test</button>
+        <!-- <p>{{bracket}}</p> -->
+    </div>
+  
+</template>
+
+<script>
+import Bracket from "@/components/bracket-components/Bracket.vue";
+export default {
+  props:{
+    competitors: Array
+  },
+  components: {
+    Bracket
+  },
+  data() {
+    return {
+      rounds: [{
+            games: [
+                {
+                    player1: { name: "?", winner: false },
+                    player2: {  name: "?", winner: false }
+                },
+                {
+                    player1: {  name: "?", winner: false },
+                    player2: {  name: "?", winner: false }
+                },
+                {
+                    player1: {  name: "?", winner: false },
+                    player2: {  name: "?", winner: false }
+                },
+                {
+                    player1: {  name: "?", winner: false },
+                    player2: {  name: "?", winner: false }
+                }
+            ]
+        },
+        //Semi
+        {
+            games: [
+                {
+                    player1: {  name: "?", winner: false },
+                    player2: {  name: "?", winner: false }
+                },
+                {
+                    player1: {  name: "?", winner: false },
+                    player2: {  name: "?", winner: false }
+                }
+            ]
+        },
+        //Final
+        {
+            games: [
+                {
+                    player1: { name: "?", winner: false },
+                    player2: {  name: "?", winner: false }
+                }
+            ]
+        }]
+    }
+  },
+  created(){
+      
+  }
+}
+</script>
