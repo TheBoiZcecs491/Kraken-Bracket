178af45be70eb25c8f7d09ed754fe8ce7faefd86
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-05-11 16:09:25 -0700

    fix: updates to user management view
    - checks if the user is logged in

--- krakenbracket-frontend/src/views/UserManagementView.vue
+++ krakenbracket-frontend/src/views/UserManagementView.vue
@@ -1,9 +1,21 @@
 <template>
-    
+    <v-app>
+        <div v-if="!loggedIn">
+            <NotLoggedIn />
+        </div>
+    </v-app>
 </template>
 
 <script>
+import { authComputed } from "../store/helpers.js";
+import NotLoggedIn from "../components/NotLoggedIn.vue";
+
 export default {
-    
+    components:{
+        NotLoggedIn
+    },
+    computed: {
+        ...authComputed
+    },
 }
 </script>
