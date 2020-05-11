1da26064aa848d50d9e783b6d1e1c64b1a15b97c
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-05-11 16:21:01 -0700

    UI improvements

--- krakenbracket-frontend/src/views/UserManagementView.vue
+++ krakenbracket-frontend/src/views/UserManagementView.vue
@@ -3,6 +3,22 @@
         <div v-if="!loggedIn">
             <NotLoggedIn />
         </div>
+        <div v-else-if="loggedIn && this.$store.state.user.accountType === 'System Admin'">
+            <h1>User Management</h1>
+            <v-container>
+                <v-row>
+                    <v-col cols="2"></v-col>
+                    <v-col cols="4">
+                        <h4>Single Create</h4>
+                        <v-text-field></v-text-field>
+                    </v-col>
+                    <v-col cols="4">
+                        <h4>Single Delete</h4>
+                    </v-col>
+                    <v-col cols="2"></v-col>
+                </v-row>
+            </v-container>
+        </div>
     </v-app>
 </template>
 
@@ -15,7 +31,7 @@ export default {
         NotLoggedIn
     },
     computed: {
-        ...authComputed
+        ...authComputed,
     },
 }
 </script>
